3f5393a73bfc80e0d4431b14ee2a187b6fc142c2
Weerawut Chaiyasomboon <weerawut@live.com>
2024-02-08 14:20:24 +0700

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Weerawut Chaiyasomboon on 8/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
